35f7dc9cd5cee5541726376dd03d047f6e842652
Andrew Serbin <serbinadrian@outlook.com>
2024-10-15 12:24:08 +0300

    [SPT-643] HOME page REGEX removed

--- config/content/navContentConfig.ts
+++ config/content/navContentConfig.ts
@@ -1,40 +1,42 @@
 import type { DefaultTheme } from "vitepress";
 
 export default [
-  { text: "Home", link: "/" },
-  { text: "Docs", link: "/docs/", activeMatch: '/docs/' },
-  {
-    text: "Products",
-    items: [
+    { text: "Home", link: "/" },
+    // { text: "Home", link: "/", activeMatch: /(^$)|(^\/$)/i },
+    // { text: "Docs", link: "/docs/", activeMatch: /^\/docs.*$/i },
+    { text: "Docs", link: "/docs/", activeMatch: /^\/docs.*$/i },
     {
-        text: "Staking portal",
-        link: "https://staking.singularitynet.io/howitworks",
-    },
-    { 
-        text: "AI Marketplace", 
-        link: "https://beta.singularitynet.io/" 
-    },
-    {
-        text: "AI Publisher", 
-        link: "https://publisher.singularitynet.io/" 
-    },
-    ],
-  },
-  {
-    text: "Community",
-    items: [
-    {
-        text: "Blog",
-        link: "http://blog.singularitynet.io/",
-    },
-    { 
-        text: "Forum", 
-        link: "https://community.singularitynet.io/" 
+        text: "Products",
+        items: [
+            {
+                text: "Staking portal",
+                link: "https://staking.singularitynet.io/howitworks",
+            },
+            {
+                text: "AI Marketplace",
+                link: "https://beta.singularitynet.io/",
+            },
+            {
+                text: "AI Publisher",
+                link: "https://publisher.singularitynet.io/",
+            },
+        ],
     },
     {
-        text: "Telegram", 
-        link: "https://telegram.me/singularitynet" 
+        text: "Community",
+        items: [
+            {
+                text: "Blog",
+                link: "http://blog.singularitynet.io/",
+            },
+            {
+                text: "Forum",
+                link: "https://community.singularitynet.io/",
+            },
+            {
+                text: "Telegram",
+                link: "https://telegram.me/singularitynet",
+            },
+        ],
     },
-    ],
-  },
 ] as DefaultTheme.NavItem[];
